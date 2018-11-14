16e22f755fd2db2788392a68da53436e2af87a99
Mallory Feuer <mallory.feuer@gmail.com>
2018-11-14 18:30:12 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,16 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
-
+  def index
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def create
+    Game.create(state: ["", "", "", "", "", "", "", "", ""])
+    render root_path
+  end
 end
